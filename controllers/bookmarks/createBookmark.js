16239d12fc8020c570e57d0d32e9ca5e2fdbc46a
Greg Flynn <greg@gfly.io>
2023-05-09 22:27:08 -0500

    download favicons for bookmarks

--- controllers/bookmarks/createBookmark.js
+++ controllers/bookmarks/createBookmark.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const { grabFavicon } = require('favicongrab');
 const asyncWrapper = require('../../middleware/asyncWrapper');
 const Bookmark = require('../../models/Bookmark');
 
@@ -18,6 +22,43 @@ const createBookmark = asyncWrapper(async (req, res, next) => {
 
   if (req.file) {
     body.icon = req.file.filename;
+  }
+
+  if (!body.icon) {
+    // download that bad boi
+    const grab = await grabFavicon(body.url);
+    let icons = grab.icons;
+
+    if (icons.length > 0) {
+      // sort so idx 0 is the highest resolution available
+      icons = icons.sort((a, b) => {
+        const aSize = a.sizes ? parseInt(a.sizes) : 0;
+        const bSize = b.sizes ? parseInt(b.sizes) : 0;
+        if (isNaN(aSize) && isNaN(bSize)) {
+          return 0;
+        } else if (isNaN(aSize)) {
+          return -1;
+        } else if (isNaN(bSize)) {
+          return 1;
+        }
+        return bSize - aSize;
+      });
+      const url = icons[0].src;
+
+      // make a unique local filename but also preserve the file extension
+      const name = `${btoa(body.url)}__${path.basename(url)}`
+      const downloadPath = path.resolve(process.cwd(), "data/uploads", name);
+      const writer = fs.createWriteStream(downloadPath);
+
+      const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+      });
+
+      response.data.pipe(writer);
+      body.icon = name;
+    }
   }
 
   bookmark = await Bookmark.create(body);
